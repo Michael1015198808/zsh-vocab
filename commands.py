078bfb160b343e54c77cc1d083024ad0472a5506
tracer-Michael <1015198808@qq.com>
2019-04-21 22:33:17 +0800

    Sun Apr 21 22:33:17 CST 2019

--- commands.py
+++ commands.py
@@ -30,7 +30,7 @@ def help():
     display("--help","Display help information")
     print(r"To load a existed word list, copy it to ~/.vocab/data.json")
     print('''Format:
-        [["intern", "\u6263\u62bc\uff0c\u62d8\u7559", 0, 1]]''')
+        [["intern", "扣押，拘留", 0, 1]]''')
 def show():
     words=read()
     for word in words:
